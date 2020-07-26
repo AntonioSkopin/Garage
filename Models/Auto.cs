e5fcfaf5f57d75e94e014f115eb8a1788877b97d
AntonioSkopin <askopin@outlook.com>
2020-07-27 01:42:18 +0200

    Changed homepage & deleted all styling

--- Models/Auto.cs
+++ Models/Auto.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Garage.Models
 {
     public class Auto
     {
         public int AutoId { get; set; }
+
         [DisplayName("Kenteken")]
+        [Required]
         public string AutoKenteken { get; set; }
+
         [DisplayName("Merk")]
+        [Required]
         public string AutoMerk { get; set; }
+
         [DisplayName("Type")]
+        [Required]
         public string AutoType { get; set; }
+
         [DisplayName("Eigenaar")]
+        [Required]
         public string EigenaarNaam { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace Garage
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Shared/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -54,7 +54,7 @@ namespace Garage
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Autos}/{action=Index}/{id?}");
             });
         }
     }
